1c63e093f32f843d4505cb6e3fcd3197d5ee5ecb
Pankaj Gusain <gusainpankaj1999@gmail.com>
2020-06-07 13:22:25 +0530

    converting to async await

--- controllers/index.js
+++ controllers/index.js
@@ -3,29 +3,57 @@ const Post =require('../models/post')
 const User =require('../models/user')
 
 // partials works
-module.exports.home=function(req,res){
-    //Post.find({},function(err,posts){
-    //   return res.render('home',{
-    //        posts :posts
-    //    })
-    //})
+//module.exports.home=function(req,res){
+    //basic way of getting the data
+        //Post.find({},function(err,posts){
+        //   return res.render('home',{
+        //        posts :posts
+        //    })
+        //})
 
     // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path :'comments',
-        populate:{
-            path :'user'
-        }
-    })
-    .exec(function(err,posts){
-       
-        User.find({},function(err,users){
-            return res.render('home',{
-                posts :posts,
-                all_users :users
-            })
+    //Post.find({})
+    //.populate('user')
+    //.populate({
+    //    path :'comments',
+    //    populate:{
+     //       path :'user'
+     //   }
+    //})
+    //.exec(function(err,posts){
+    //   
+     //   User.find({},function(err,users){
+    //        return res.render('home',{
+     //           posts :posts,
+     //           all_users :users
+    //        })
+    //    })
+    //})
+//}
+
+    // using async await to make a cleaner code
+module.exports.home= async function(req,res){
+
+    try{
+        let posts=await Post.find({})
+            .populate('user')
+            .populate({
+                path :'comments',
+                populate:{
+                    path :'user'
+                }
+            });
+
+        let users=await User.find({});
+            
+        return res.render('home',{
+        posts :posts,
+        all_users :users
         })
-    })
+
+    }catch(err){
+
+        console.log("error",err);
+        return
+    }
 }
--- controllers/post.js
+++ controllers/post.js
@@ -4,19 +4,33 @@ const Comment =require('../models/comment');
 
 
 //to add post or comment in the database
-module.exports.post_content =function(req,res){
-    console.log(req.body)
-        Post.create({
-            content : req.body.content,
-            user :req.user._id
-        },function(err){
-            if(err){
-                console.log('error in creating user while posting');
-                return
-            }
-                return res.redirect('back');
+module.exports.post_content =async function(req,res){
+    // normal method    
+    //Post.create({
+        //    content : req.body.content,
+        //    user :req.user._id
+        //},function(err){
+        //    if(err){
+        //        console.log('error in creating user while posting');
+        //        return
+        //    }
+        //        return res.redirect('back');
+        //}
+        //)
+
+        //using async wait method
+        try{
+            await Post.create({
+                   content : req.body.content,
+                    user :req.user._id
+                })
+            return res.redirect('back');
+        }catch(err){
+            console.log('Error',err);
+            return
+
         }
-    )
+
 }
 
 // to delete a post and comments associated with it
@@ -35,4 +49,23 @@ module.exports.destroy = function(req,res){
             return res.redirect('back');
         }
     })
+
+
 }
+
+ // to delete a post and comments associated with it  but this time using async wait method
+    //module.exports.destroy = async function(req,res){
+    //    try{
+    //        let post= await Post.findById(req.params.id);
+
+    //        if(post.user == req.user.id){
+    //            post.remove();
+
+    //            await  Comment.deleteMany({post: req.params.id});
+    //            return res.redirect('back');
+    //            }
+    //    }catch(err){
+    //        console.log("error",err);
+    //        return
+    //}
+//}
